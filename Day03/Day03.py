7706b1db45f04dedb9d2d3deefdbbb4ccdee93e2
Graham Preston <graham.preston@gmail.com>
2016-12-04 15:35:28 -0500

    Day 3 part 2

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -12,7 +12,18 @@ def main():
         if triangle[0] + triangle[1] > triangle[2]:
             count += 1
     
-    print(count)
+    print("Possible triangles (part 1):", count)
+    
+    count = 0
+    for c in range(len(sides[0])):
+        r = 0
+        while r < len(sides):
+            current = sorted([sides[r][c], sides[r+1][c], sides[r+2][c]])
+            if current[0] + current[1] > current[2]:
+                count += 1
+            r += 3
+    
+    print("Possible triangles (part 2):", count)
     
 if __name__ == '__main__':
     main()
