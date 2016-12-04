8b2f7a339f88e8f022fb4ea409174f7e58855ac2
Graham Preston <graham.preston@gmail.com>
2016-12-04 13:48:06 -0500

    Added part 2 to day 1

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -8,6 +8,7 @@ facing = 0
 moves = {0: [0, 1], 1: [1, 0], 2: [0, -1], 3: [-1, 0]}
 coordinates = [0, 0]
 locations = [[0, 0]]
+repeated = False
 for d in directions:
     if d[0] == "R":
         if facing == 3:
@@ -23,5 +24,10 @@ for d in directions:
     for n in range(int(d[1:])):
         coordinates = [x + y for x, y in zip(coordinates, moves[facing])]
         
-print(coordinates)
-print(int(math.fabs(coordinates[0] + coordinates[1])), "blocks away.")
+        if coordinates in locations and repeated == False:
+            print("First repeat at", str(coordinates) + ",", int(math.fabs(coordinates[0]) + math.fabs(coordinates[1])), "blocks away.")
+            repeated = True
+        else:
+            locations.append(list(coordinates))
+        
+print("End at", str(coordinates) + ",", int(math.fabs(coordinates[0]) + math.fabs(coordinates[1])), "blocks away.")
