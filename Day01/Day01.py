d3e1450c1f6d4c7f4e90be82b71a4e1c0409d8e6
Graham Preston <graham.preston@gmail.com>
2016-12-04 13:38:50 -0500

    Improved moving system

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -5,7 +5,9 @@ with open("Day01_Input.txt") as file:
 
 # The direction being faced, 0 = North, 1 = East, 2 = South, 3 = West
 facing = 0
+moves = {0: [0, 1], 1: [1, 0], 2: [0, -1], 3: [-1, 0]}
 coordinates = [0, 0]
+locations = [[0, 0]]
 for d in directions:
     if d[0] == "R":
         if facing == 3:
@@ -18,14 +20,8 @@ for d in directions:
         else:
             facing -= 1
             
-    if facing == 0:
-        coordinates[1] += int(d[1:])
-    elif facing == 1:
-        coordinates[0] += int(d[1:])
-    elif facing == 2:
-        coordinates[1] -= int(d[1:])
-    else:
-        coordinates[0] -= int(d[1:])
+    for n in range(int(d[1:])):
+        coordinates = [x + y for x, y in zip(coordinates, moves[facing])]
         
 print(coordinates)
 print(int(math.fabs(coordinates[0] + coordinates[1])), "blocks away.")
