0c26a36cce21929d04939a4925311e0d313310db
Graham Preston <graham.preston@gmail.com>
2016-12-09 16:43:14 -0500

    Day 7 part 2 (unfinished)

--- Day09/Day09.py
+++ Day09/Day09.py
@@ -1,11 +1,13 @@
 def main():
     with open('Day09_Input.txt') as file:
-        global contents
         contents = file.read()
     
+    part2(contents)
+    
+def part1(contents):
     index = 0
     while index < len(contents):
-        if contents[index] == '(' and is_marker(index):
+        if contents[index] == '(' and is_marker(index, contents):
             m = 1
             while contents[index+m] != ')':
                 m += 1
@@ -17,7 +19,24 @@ def main():
             index += 1
     print(len(contents))
     
-def is_marker(i):
+def part2(contents):
+    index = 0
+    while index < len(contents):
+        if contents[index] == '(' and is_marker(index, contents):
+            m = 1
+            while contents[index+m] != ')':
+                m += 1
+            nums = [int(n) for n in contents[index+1:index+m].split('x')]
+            sub = contents[index+m+1:index+m+1+nums[0]]
+            contents = contents[:index] + (nums[1] * sub) + contents[index+m+1+nums[0]:]
+        else:
+            index += 1
+        print(len(contents))
+        
+def find_length(s):
+    
+    
+def is_marker(i, contents):
     n = 1
     while contents[i+n] != ')':
         n += 1
