8fdd24d8907f80fd77dfb307d55f451b5583548a
Graham Preston <graham.preston@gmail.com>
2016-12-21 11:33:34 -0500

    Day 21 part 2

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -1,10 +1,39 @@
+from itertools import permutations
 def main():
-    with open("Day21_Input.txt") as file:
-        contents = [line.rstrip().split(' ') for line in reversed(file.readlines())]
+    with open('Day21_Input.txt') as file:
+        contents = [line.rstrip().split(' ') for line in file.readlines()]
     
     password = 'fbgdceah'
+    
+    brute_unscramble(contents, password)
 
+def scramble(contents, password):
     for line in contents:
+        if line[0] == 'swap':
+            if line[1] == 'position':
+                password = swap_pos(password, int(line[2]), int(line[5]))
+            else:
+                password = swap_letters(password, line[2], line[5])
+        elif line[0] == 'rotate':
+            if line[1] == 'right':
+                password = rotate_right(password, int(line[2]))
+            elif line[1] == 'left':
+                password = rotate_left(password, int(line[2]))
+            else:
+                if password.index(line[6]) >= 4:
+                    password = rotate_right(password, 2 + int(password.index(line[6])))
+                else:
+                    password = rotate_right(password, 1 + int(password.index(line[6])))
+        elif line[0] == 'reverse':
+            password = reverse(password, int(line[2]), int(line[4]))
+        else:
+            password = move(password, int(line[2]), int(line[5]))
+    
+    return password
+
+# Not working, brute_unscramble() works
+def unscramble(contents, password):
+    for line in reversed(contents):
         if line[0] == 'swap':
             if line[1] == 'position':
                 password = swap_pos(password, int(line[2]), int(line[5]))
@@ -16,16 +45,18 @@ def main():
             elif line[1] == 'left':
                 password = rotate_right(password, int(line[2]))
             else:
-                if int(password.index(line[6])) >= 4:
-                    password = rotate_left(password, 2 + int(password.index(line[6])))
-                else:
-                    password = rotate_right(password, 1 + int(password.index(line[6])))
+                password = unrotate(password, line[6])
         elif line[0] == 'reverse':
             password = reverse(password, int(line[2]), int(line[4]))
         else:
-            password = move(password, int(line[2]), int(line[5]))
+            password = move(password, int(line[5]), int(line[2]))
     
-    print(password)
+    return password
+
+def brute_unscramble(contents, password):
+    for s in [''.join(p) for p in permutations(password)]:
+        if scramble(contents, s) == password:
+            print(s)
 
 def swap_pos(s, x, y):
     if x < y:
@@ -49,6 +80,16 @@ def rotate_left(s, n):
 def rotate_right(s, n):
     n = n % len(s)
     return s[-n:] + s[:-n]
+
+def unrotate(s, a):
+    for i in range(1, len(s) + 1):
+        test_pass = rotate_left(s, i)
+        if test_pass.index(a) >= 4:
+            if s == rotate_right(test_pass, 2 + int(test_pass.index(a))):
+                return test_pass
+        else:
+            if s == rotate_right(test_pass, 1 + int(test_pass.index(a))):
+                return test_pass
 
 def reverse(s, x, y):
     if x < y:
