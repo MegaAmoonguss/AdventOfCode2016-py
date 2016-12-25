29bdd0a98dc4b73f01247b25367345954af4bdc4
Graham Preston <graham.preston@gmail.com>
2016-12-25 01:02:09 -0500

    Day 13 almost working

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -1,6 +1,6 @@
 def main():
-    favorite = 1358
-    dim = 100
+    favorite = 10
+    dim = 50
     global building
     building = [[' ' for _ in range(dim)] for _ in range(dim)]
     
@@ -12,10 +12,7 @@ def main():
                 if c == '1':
                     ones += 1
             if ones % 2 == 1:
-                if x == 31 and y == 39:
-                    building[y][x] = 'X'
-                else:
-                    building[y][x] = '#'
+                building[y][x] = '#'
             else:
                 building[y][x] = '.'
     
@@ -26,42 +23,39 @@ def main():
             else:
                 print(building[r][c], end='')
         print()
+        
+    global graph
+    graph = {}
+    print(len(bfs(graph, (1, 1), (31, 39))) - 1)
 
-#===============================================================================
-# def move_dfs(coordinates, adj, length):
-#     if coordinates == [39, 31]:
-#         return length
-#     
-#     adj = [False, False, False, False]
-#     if building[coordinates[0]][coordinates[1] - 1] == '.':
-#         adj[0] = True
-#     if building[coordinates[0]][coordinates[1] + 1] == '.':
-#         adj[1] = True
-#     if building[coordinates[0] - 1][coordinates[1]] == '.':
-#         adj[2] = True
-#     if building[coordinates[0] + 1][coordinates[1]] == '.':
-#         adj[3] = True
-#      
-#     if len(path) < 150:
-#         if not True in doors:
-#             print('FAILURE: ', path)
-#         elif occurrences(True, doors) == 1:
-#             index = doors.index(True)
-#             coordinates = [x + y for x, y in zip(coordinates, moves[index])]
-#             path += directions[index]
-#             move_dfs(coordinates, path, input, doors)
-#         else:
-#             for i in reversed(range(len(doors))):
-#                 if doors[i] == True:
-#                     move_dfs([x + y for x, y in zip(coordinates, moves[i])], path + directions[i], input, doors)
-#      
-# def occurrences(e, l):
-#     count = 0
-#     for o in l:
-#         if o == e:
-#             count += 1
-#     return count
-#===============================================================================
+def bfs(graph, start, end):
+    # maintain a queue of paths
+    queue = []
+    # push the first path into the queue
+    queue.append([start])
+    while queue:
+        # get the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        # path found
+        if node == end:
+            return path
+        # add adjacents to graph
+        graph[node] = []
+        if building[node[1]][node[0] + 1] == '.' and (node[0] + 1, node[1]) not in path:
+            graph[node].append((node[0] + 1, node[1]))
+        if building[node[1] + 1][node[0]] == '.' and (node[0], node[1] + 1) not in path:
+            graph[node].append((node[0], node[1] + 1))
+        if node[0] != 0 and building[node[1]][node[0] - 1] == '.' and (node[0] - 1, node[1]) not in path:
+            graph[node].append((node[0] - 1, node[1]))
+        if node[1] != 0 and building[node[1] - 1][node[0]] == '.' and (node[0], node[1] - 1) not in path:
+            graph[node].append((node[0], node[1] - 1))
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        for adjacent in graph.get(node, []):
+            new_path = list(path)
+            new_path.append(adjacent)
+            queue.append(new_path)
 
 if __name__ == '__main__':
     main()
